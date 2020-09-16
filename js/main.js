6b4f0f920ff2a6e179c20036ce6da413d9a1b153
andyvasquez-dev <andyvasquez.dev@gmail.com>
2020-09-15 23:55:09 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -165,8 +165,8 @@ function showResults(results){
 function movieSearch(){ // bring back all matches dating back to 1995
   document.querySelector('#output').classList.remove('hidden')
     document.querySelector('.loader').style.display='flex'
-  // }
-  const yearCheckEnd = 2005
+  // }14
+  const yearCheckEnd = 2015
   const currentYear = 2020
   const searchResults = []
 
